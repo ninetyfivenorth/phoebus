5708581bd4e3ae4b3324ac0317754eb1f842840b
Matt A. Tobin <email@mattatobin.com>
2016-10-26 01:06:10 -0400

    Move funcReadAddonManifest() to common functions

--- backend/components/aus/aus.php
+++ backend/components/aus/aus.php
@@ -7,49 +7,6 @@
 
 // ============================================================================
 
-// == | funcReadAddonManifest | ===============================================
-
-function funcReadAddonManifest($_addonType, $_addonSlug, $_mode) {
-    $_addonManifest = parse_ini_file('../datastore/' . $_addonType . 's/' . $_addonSlug . '/manifest.ini');
-    
-    if ($_addonManifest == false) {
-        funcError('Unable to read manifest ini file');
-    }
-    else {
-        // $_mode 0 = undefined (future for fe) 1 = aus, 2 = download
-        if ($_mode == 1) {
-            unset($_addonManifest['id']);
-            unset($_addonManifest['compat']);
-            unset($_addonManifest['name']);
-            unset($_addonManifest['author']);
-            unset($_addonManifest['description']);
-
-            $_addonManifest["hash"] = hash_file('sha256', '../datastore/' . $_addonType . 's/' . $_addonSlug . '/' . $_addonManifest["xpi"]);
-
-            if ($_SERVER["HTTP_X_FORWARDED_HOST"] == 'dev.addons.palemoon.org') {
-                $_addonManifest["baseurl"] = 'https://dev.addons.palemoon.org/datastore/' . $_addonType . 's/' . $_addonSlug . '/' . $_addonManifest['xpi'];
-            }
-            else {
-                $_addonManifest["baseurl"] = 'https://addons.palemoon.org/phoebus/datastore/' . $_addonType . 's/' . $_addonSlug . '/' . $_addonManifest['xpi'];
-            }
-        }
-        elseif ($_mode == 2) {
-            unset($_addonManifest['id']);
-            unset($_addonManifest['type']);
-            unset($_addonManifest['compat']);
-            unset($_addonManifest['minVer']);
-            unset($_addonManifest['maxVer']);
-            unset($_addonManifest['name']);
-            unset($_addonManifest['author']);
-            unset($_addonManifest['description']);
-        }
-
-        return $_addonManifest;
-    }
-}
-
-// ============================================================================
-
 // == | funcGenerateUpdateXML | ===============================================
 
 function funcGenerateUpdateXML($_addonManifest) {
