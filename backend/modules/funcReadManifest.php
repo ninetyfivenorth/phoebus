f66564ce6d7c8b5ea6e0c252d5bd0062240a8191
Matt A. Tobin <email@mattatobin.com>
2016-10-27 19:59:56 -0400

    ewwwww

--- backend/modules/funcReadManifest.php
+++ backend/modules/funcReadManifest.php
@@ -26,8 +26,7 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode, $_useNewManifest) {
                 $_addonPhoebusContent = htmlentities($_addonPhoebusContent, ENT_XHTML);
                 $_addonPhoebusContent = str_replace("\n", "<br />\n", $_addonPhoebusContent);
                 $_addonPhoebusContent = str_replace('&lt;img', '<img', $_addonPhoebusContent);
-                $_addonPhoebusContent = preg_replace('/&lt;a (.*)&gt;/', '<a $1>', $_addonPhoebusContent);
-                $_addonPhoebusContent = str_replace('&lt;/a&gt;', '</a>', $_addonPhoebusContent);
+                $_addonPhoebusContent = preg_replace('&lt;a (.*)&gt;(.*)&lt;\/a&gt;', '<a $1>$2</a>', $_addonPhoebusContent);
                 $_addonPhoebusContent = str_replace('/&gt;', '/>', $_addonPhoebusContent);
                 $_addonManifest['metadata']['LongDescription'] = $_addonPhoebusContent;
             }
