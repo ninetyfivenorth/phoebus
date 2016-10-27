9d9fcab22436deaa9e6d7088bc823a1583b001a6
Matt A. Tobin <email@mattatobin.com>
2016-10-27 19:34:38 -0400

    fse

--- backend/modules/funcReadManifest.php
+++ backend/modules/funcReadManifest.php
@@ -24,7 +24,7 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode, $_useNewManifest) {
             if (file_exists($_addonBasePath . $_addonPhoebusContentFile)) {
                 $_addonPhoebusContent = file_get_contents($_addonBasePath . $_addonPhoebusContentFile);
                 $_addonPhoebusContent = htmlentities($_addonPhoebusContent, ENT_XHTML);
-                $_addonPhoebusContent = str_replace("\n", "\n<br />", $_addonPhoebusContent);
+                $_addonPhoebusContent = str_replace("\n", "<br />\n", $_addonPhoebusContent);
                 $_addonPhoebusContent = str_replace('&lt;img', '<img', $_addonPhoebusContent);
                 $_addonPhoebusContent = str_replace('/&gt', '/>', $_addonPhoebusContent);  
                 $_addonPhoebusContent = str_replace('&lt;a', '<a', $_addonPhoebusContent);
