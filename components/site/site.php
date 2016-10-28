95638877dc1582af9e8cbb27ba2b264396c46e57
Matt A. Tobin <email@mattatobin.com>
2016-10-28 03:29:02 -0400

    few

--- components/site/site.php
+++ components/site/site.php
@@ -1,7 +1,14 @@
 <?php
 
+if ($_SERVER['REQUEST_URI'] == '/') {
+    $strRequestPath = "/";
+}
+else {
+    $strRequestPath = funcHTTPGetValue('path');
+}
+
 header('Content-Type: text/plain');
-print( $_SERVER['REQUEST_URI'] . "\n" . $strRequestPage . "\n");
+print( $_SERVER['REQUEST_URI'] . "\n" . $strRequestPath . "\n");
 $parsed = parse_url($_SERVER['REQUEST_URI']);
 var_dump($parsed);
 
