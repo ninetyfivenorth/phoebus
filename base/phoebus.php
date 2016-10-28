633ae12bc58a9f9ac4ff605442eade9cd3f8a7af
Matt A. Tobin <email@mattatobin.com>
2016-10-28 05:26:51 -0400

    fs

--- base/phoebus.php
+++ base/phoebus.php
@@ -30,7 +30,7 @@ $arrayModules = array(
 );
 
 $strRequestComponent = funcHTTPGetValue('component');
-$arrayArgs = explode('$', implode('$', preg_grep('/^component=(.*)/', explode('&', parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY)))));
+$arrayArgs = explode('&', implode('&', preg_grep('/^component=(.*)/', explode('&', parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY)))));
 $strRequestPath = funcHTTPGetValue('path');
 
 // ============================================================================
