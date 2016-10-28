d5077193ce3946dd48f1135ecfadaeeb435f329e
Matt A. Tobin <email@mattatobin.com>
2016-10-27 23:53:12 -0400

    comments

--- backend/modules/funcReadManifest.php
+++ backend/modules/funcReadManifest.php
@@ -11,38 +11,62 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode, $_useNewManifest) {
     if ($_useNewManifest == true && file_exists($_addonBasePath . $_addonPhoebusManifestFile)) {
         $_addonManifest = parse_ini_file($_addonBasePath . $_addonPhoebusManifestFile, true);
         if ($_addonManifest != false) {
+            // INI has depth and identical section name issues so we need to mangle it
+            // Create a temporary array that we can easily manipulate
             $_addonManifestVersions = $_addonManifest;
+            
+            // Drop the addon and metadata keys off the temporary array
             unset($_addonManifestVersions['addon']);
             unset($_addonManifestVersions['metadata']);
             
+            // mangle filename.xpi sections into a subkey
+            // we are now working on the add-on manifest array
             foreach ($_addonManifestVersions as $_key => $_value) {
                 unset($_addonManifest[$_key]);
                 $_addonManifest['xpi'][$_key] = $_value;
             }
+            
+            // clear the temporary array out of memory
             unset($_addonManifestVersions_);
             
+            // Parse phoebus.content
             if (file_exists($_addonBasePath . $_addonPhoebusContentFile)) {
+                // Read phoebus.content
                 $_addonPhoebusContent = file_get_contents($_addonBasePath . $_addonPhoebusContentFile);
+                
+                // html encode phoebus.content
                 $_addonPhoebusContent = htmlentities($_addonPhoebusContent, ENT_XHTML);
+                
+                // normalize line endings
                 $_addonPhoebusContent = str_replace("\r\n", "\n", $_addonPhoebusContent);
+                
+                // automagically turn newlines into <br />
                 $_addonPhoebusContent = str_replace("\n", "<br />\n", $_addonPhoebusContent);
                 
-                $arrayPhoebusCode = array(
+                // create a temporary array that contains the regex to convert pseudo-bbcode to real html
+                $_arrayPhoebusCode = array(
                     '\[url="(.*)"\](.*)\[\/url\]' => '<a href="$1" target="_blank">$2</a>',
                     '\[url\](.*)\[\/url\]' => '<a href="$1" target="_blank">$1</a>',
                     '\[img(.*)\](.*)\[\/img\]' => '<img src="$2"$1 />'
                 );
-
-                foreach ($arrayPhoebusCode as $_key => $_value) {
+                
+                // Regex replace pseudo-bbcode with real html
+                foreach ($_arrayPhoebusCode as $_key => $_value) {
                     $_addonPhoebusContent = preg_replace('/' . $_key . '/iU', $_value, $_addonPhoebusContent);
                 }
-
+                
+                // Clear the temporary array out of memory
+                unset($_arrayPhoebusCode)
+                
+                // Assign parsed phoebus.content to the add-on manifest array
                 $_addonManifest['metadata']['longDescription'] = $_addonPhoebusContent;
             }
             else {
+                // Since there is no phoebus.content use the short description
                 $_addonManifest['metadata']['longDescription'] = $_addonManifest['metadata']['shortDescription'];
             }
             
+            // Generate a sha256 hash on the fly for the add-on
             if (file_exists($_addonBasePath . $_addonManifest['addon']['release'])) {    
                 $_addonManifest['addon']['hash'] = hash_file('sha256', $_addonBasePath . $_addonManifest['addon']['release']);
             }
@@ -50,7 +74,10 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode, $_useNewManifest) {
                 funcError('Could not find ' . $_addonManifest["xpi"]);
             }
             
+            // assign the baseURL to the add-on manifest array
             $_addonManifest['addon']["baseURL"] = 'https://dev.addons.palemoon.org/datastore/' . $_addonDirectory;
+            
+            // We are using the new manifest
             $_addonManifest['isNewManifest'] = true;
             return $_addonManifest;
         }
