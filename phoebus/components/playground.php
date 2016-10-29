76c23b550ab72379f0c9e98e8c0ef7ad801d8537
Matt A. Tobin <email@mattatobin.com>
2016-10-29 17:42:26 -0400

    fes

--- phoebus/components/playground.php
+++ phoebus/components/playground.php
@@ -9,45 +9,8 @@ $strRequestMode = funcHTTPGetValue('mode');
 
 // ============================================================================
 
-// ============================================================================
-
-include_once($arrayModules['readManifest']);
-
-if ($strRequestMode == null) {
-    funcError('Mode is null.. Dumbass');
-}
-
-if ($strRequestMode == 'manifest') {
-    header('Content-Type: text/plain');
-    
-    var_dump(funcReadManifest('extension', 'adblock-latitude', 0, true));
-}
-elseif ($strRequestMode == 'convert') {
-    header('Content-Type: text/plain');
-    
+function fumcConvertAddon($_type, $_slug) {
     $addonManifest = funcReadManifest('extension', 'adblock-latitude', 0, false);
-    //var_dump($addonManifest);
-    //print("\n");
-    $addonNewManifest = array(
-        'addon' => array(
-                    'type' => $addonManifest['type'],
-                    'id' => $addonManifest['guid'],
-                    'release' => $addonManifest['xpi'],
-                    'unstable' => 'none'),
-        'metadata' => array(
-                    'name' => $addonManifest['name'],
-                    'slug' => 'unknown',
-                    'author' => $addonManifest['author'],
-                    'shortDescription' => 'none',
-                    'licence' => 'none',
-                    'homepageURL' => 'none',
-                    'supportURL' => 'none'),
-        'xpi' => array(
-                    $addonManifest['xpi'] => array(
-                        'version' => $addonManifest['version'],
-                        'minAppVersion' => $addonManifest['minVer'],
-                        'maxAppVersion' => $addonManifest['minVer'])),
-    );
     
     $addonNewManifestINI = '[addon]
 type="' . $addonManifest['type'] . '"
@@ -57,7 +20,7 @@ unstable="none"
 
 [metadata]
 name="' . $addonManifest['name'] . '"
-slug="' . 'unknown' . '"
+slug="' . $_slug . '"
 author="' . $addonManifest['author'] . '"
 shortDescription="' . 'unknown' . '"
 licence="none"
@@ -68,13 +31,32 @@ supportURL="none"
 version="' . $addonManifest['version'] . '"
 minAppVersion="' . $addonManifest['minVer'] . '"
 maxAppVersion="' . $addonManifest['maxVer'] . '"';
-    print($addonNewManifestINI);
-    //print("\n\n");
+
     $addonContent = $addonManifest["description"];
     $addonContent = str_replace('<p>', '', $addonContent);
     $addonContent = str_replace('<br />', "\n", $addonContent);
     $addonContent = str_replace('</p>', "\n", $addonContent);
-    //var_dump($addonContent);
+    
+    return array($addonNewManifestINI, $addonContent);
+}
+
+// ============================================================================
+
+include_once($arrayModules['readManifest']);
+
+if ($strRequestMode == null) {
+    funcError('Mode is null.. Dumbass');
+}
+
+if ($strRequestMode == 'manifest') {
+    header('Content-Type: text/plain');
+    var_dump(fumcConvertAddon('extension', 'adblock-latitude'));
+
+}
+elseif ($strRequestMode == 'convert') {
+    header('Content-Type: text/plain');
+    
+
 }
 else {
     funcError('Invalid Mode');
