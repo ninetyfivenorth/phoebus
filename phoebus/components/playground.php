7f94df43edd84ce2e6a072e80d1ede15fdbc6994
Matt A. Tobin <email@mattatobin.com>
2016-10-29 19:48:38 -0400

    tre

--- phoebus/components/playground.php
+++ phoebus/components/playground.php
@@ -57,7 +57,7 @@ maxAppVersion="' . $addonManifest['maxVer'] . '"';
     $addonContent = str_replace('</a>', "[/url]", $addonContent);
     $addonContent = str_replace(' target=_blank', '', $addonContent);
     $addonContent = preg_replace('/<a href=(.*)>/Ui', '[url=$1]', $addonContent);
-    $addonContent = preg_replace('/\[url=\"(.*)\"\]', '[url=$1]', $addonContent);
+    $addonContent = preg_replace('/\[url=\"(.*)\"\]/Ui', '[url=$1]', $addonContent);
     return array($addonNewManifestINI, $addonContent);
 
 }
