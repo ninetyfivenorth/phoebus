eb390110bf006dba769d7500c2da9fc359b7084e
Matt A. Tobin <email@mattatobin.com>
2016-10-29 15:57:23 -0400

    fes

--- phoebus/components/aus/aus.php
+++ phoebus/components/aus/aus.php
@@ -48,7 +48,7 @@ function funcGenerateUpdateXML($_addonManifest) {
                     '@PALEMOON_ID@' => $GLOBALS['strPaleMoonID'],
                     '@ADDON_MINVERSION@' => $_addonManifest['xpi'][$_addonManifest['addon']['release']]['minAppVersion'],
                     '@ADDON_MAXVERSION@' => $_addonManifest['xpi'][$_addonManifest['addon']['release']]['maxAppVersion'],
-                    '@ADDON_XPI@' => $_addonManifest['baseURL'] . $_addonManifest['xpi'],
+                    '@ADDON_XPI@' => $_addonManifest['addon']['baseURL'] . $_addonManifest['addons']['release'],
                     '@ADDON_HASH@' => $_addonManifest['addon']['hash']
             );
             }
