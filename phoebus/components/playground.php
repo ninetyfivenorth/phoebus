5c9676cc3e0438d2bbb304bebd0d58e9fe4acacf
Matt A. Tobin <email@mattatobin.com>
2016-10-29 18:40:50 -0400

    fes

--- phoebus/components/playground.php
+++ phoebus/components/playground.php
@@ -53,7 +53,8 @@ maxAppVersion="' . $addonManifest['maxVer'] . '"';
     $addonContent = str_replace('</em>', "[/i]", $addonContent);
     $addonContent = str_replace('<u>', "[/u]", $addonContent);
     $addonContent = str_replace('</u>', "[/u]", $addonContent);
-    $addonContent = str_replace('/<a href=(.*)\ (.*)>(.*)<\/a>/Ui', '[url="$1"]$3[/url]', $addonContent);
+    $addonContent = str_replace(' target="blank"', "", $addonContent);
+    $addonContent = str_replace('/<a href=(.*)>(.*)<\/a>/Ui', '[url="$1"]$2[/url]', $addonContent);
     return array($addonNewManifestINI, $addonContent);
 
 }
