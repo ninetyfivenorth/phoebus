ae06f89d8ebb5f07e92165a4cf4caa5c1232525c
Matt A. Tobin <email@mattatobin.com>
2016-10-30 00:00:28 -0400

    gr4egre

--- phoebus/components/playground.php
+++ phoebus/components/playground.php
@@ -31,7 +31,7 @@ elseif ($strRequestMode == 'extensions') {
     include_once($arrayModules['dbExtensions']);
     
     foreach ($arrayExtensionsDB as $_key => $_value) {
-        var_dump(funcReadManifest('extension', $strRequestSlug, false, false, true, true));
+        var_dump(funcReadManifest('extension', $_value, false, false, true, true));
     }
 
 }
