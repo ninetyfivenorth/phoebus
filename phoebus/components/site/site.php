913d25eb389a33ec8422f522b0cc68594e3b46f1
Matt A. Tobin <email@mattatobin.com>
2016-10-30 01:09:28 -0400

    grd

--- phoebus/components/site/site.php
+++ phoebus/components/site/site.php
@@ -1,7 +1,40 @@
 <?php
-header('Content-Type: text/plain');
-print( $_SERVER['REQUEST_URI'] . "\n" . $strRequestPath . "\n");
-$parsed = parse_url($_SERVER['REQUEST_URI']);
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL
 
+// == | Vars | ================================================================
 
+$arraySections = array(
+    'pages' => './phoebus/components/site/pages.php',
+    'extensions' => './phoebus/components/site/extensions.php',
+    'themes' => './phoebus/components/site/themes.php',
+    'searchplugins' => './phoebus/components/site/searchplugins.php'
+);
+
+// ============================================================================
+
+// == | Main | ================================================================
+
+if (startsWith($strRequestPath, '/') ) {
+    header('Content-Type: text/plain');
+    print('homepage');
+}
+elseif (startsWith($strRequestPath, '/extensions/')) {
+    header('Content-Type: text/plain');
+    print('extensions');
+}
+elseif (startsWith($strRequestPath, '/themes/')) {
+    header('Content-Type: text/plain');
+    print('themes');
+}
+elseif (startsWith($strRequestPath, '/searchplugins/')) {
+    header('Content-Type: text/plain');
+    print('searchplugins');
+}
+else {
+    header("HTTP/1.0 404 Not Found");
+}
+
+// ============================================================================
 ?>
--- phoebus/modules/common.php
+++ phoebus/modules/common.php
@@ -39,4 +39,23 @@ function funcRedirect($_strURL) {
 }
 
 // ============================================================================
+
+// == | Functions: startsWith & endsWith |=====================================
+function startsWith($haystack, $needle)
+{
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+}
+
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+// ============================================================================
 ?>
