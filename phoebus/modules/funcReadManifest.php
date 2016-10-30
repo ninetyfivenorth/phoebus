bdd0d0ec7e5db591d13ecd39ef35f8940120a96e
Matt A. Tobin <email@mattatobin.com>
2016-10-29 22:33:43 -0400

    fesfes

--- phoebus/modules/funcReadManifest.php
+++ phoebus/modules/funcReadManifest.php
@@ -8,7 +8,7 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode) {
     $_addonPhoebusManifestFile = 'phoebus.manifest';
     $_addonPhoebusContentFile = 'phoebus.content';
     
-    if ($_useNewManifest == true && file_exists($_addonBasePath . $_addonPhoebusManifestFile)) {
+    if (file_exists($_addonBasePath . $_addonPhoebusManifestFile)) {
         $_addonManifest = parse_ini_file($_addonBasePath . $_addonPhoebusManifestFile, true);
        
         // INI has depth and identical section name issues so we need to mangle it
@@ -86,48 +86,6 @@ function funcReadManifest($_addonType, $_addonSlug, $_mode) {
     }
     else {
         funcError('Unable to read manifest file');
-    }
-
-        
-        ///old
-    elseif ($_useNewManifest == false && file_exists($_addonBasePath . $_addonManifestINIFile)) {
-        $_addonManifest = parse_ini_file($_addonBasePath . $_addonManifestINIFile);
-        if ($_addonManifest != false) {
-            // $_mode 0 = (future for fe) 1 = aus, 2 = download
-            switch ($_mode) {
-                case 0:
-                    $_arrayUnsetKeys = null;
-                    break;
-                case 1:
-                    $_arrayUnsetKeys = array('id', 'compat', 'name', 'author', 'description');
-                    if (file_exists($_addonBasePath . $_addonManifest["xpi"])) {    
-                        $_addonManifest["hash"] = hash_file('sha256', $_addonBasePath . $_addonManifest["xpi"]);
-                    }
-                    else {
-                        funcError('Could not find ' . $_addonManifest["xpi"]);
-                    }
-                    
-
-                    break;
-                case 2:
-                    $_arrayUnsetKeys = array('id', 'compat', 'minVer', 'maxVer', 'name', 'author', 'description');
-                    $_addonManifest['basepath'] = $_addonBasePath;
-                    break;
-            }
-            if ($_arrayUnsetKeys != null) {
-                foreach ($_arrayUnsetKeys as $_value) {
-                    unset($_addonManifest[$_value]);
-                }
-            }
-            $_addonManifest['isNewManifest'] = false;
-            return $_addonManifest;
-        }
-        else {
-            funcError('Unable to read manifest file');
-        }
-    }
-    else {
-        funcError('Unable to find manifest file');
     }
 }
 
