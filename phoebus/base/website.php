e7a927fb5b3e3aa2d22c80784fb2b6e9fead919b
Matt A. Tobin <email@mattatobin.com>
2016-11-01 00:45:57 -0400

    gre

--- phoebus/base/website.php
+++ phoebus/base/website.php
@@ -12,11 +12,12 @@ $arraySections = array(
 
 // ============================================================================
 
-// == | funcHeader | ================================================================
+// == | funcHeader | ==========================================================
 
 function funcSendHeader($_value) {
     if ($_value == '404') {
         header("HTTP/1.0 404 Not Found");
+        exit();
     }
     elseif ($_value == 'html') {
         header('Content-Type: text/html');
@@ -33,8 +34,6 @@ function funcSendHeader($_value) {
 
 // == | Main | ================================================================
 
-include_once($arrayModules['readManifest']);
-
 if (startsWith($strRequestPath, '/extensions/') == true ||
     startsWith($strRequestPath, '/themes/') == true ||
     startsWith($strRequestPath, '/searchplugins/') == true) {
@@ -43,68 +42,6 @@ if (startsWith($strRequestPath, '/extensions/') == true ||
 else {
     include_once($arraySections['pages']);
 }
-
-/* 
-if ($strRequestPath == '/') {
-    header('Content-Type: text/plain');
-    print('homepage');
-}
-elseif (startsWith($strRequestPath, '/extensions/')) {
-    include_once($arrayModules['dbExtensions']);
-    if ($strRequestPath == '/extensions/') {
-        header('Content-Type: text/plain');
-        print('extensions main page');
-    }
-    elseif ($strRequestPath == '/extensions/all/') {
-        header('Content-Type: text/plain');
-        foreach ($arrayExtensionsDB as $_key => $_value) {
-            var_dump(funcReadManifest('extension', $_value, true, false, false, false, false));
-        }
-    }
-    else {
-        $strStrippedPath = str_replace('/', '', str_replace('/extensions/', '', $strRequestPath));
-        $ArrayDBFlip = array_flip($arrayExtensionsDB);
-
-        if (array_key_exists($strStrippedPath,$ArrayDBFlip)) {
-            header('Content-Type: text/plain');
-            var_dump(funcReadManifest('extension', $strStrippedPath, true, true, false, false, false));
-        }
-        else {
-            header("HTTP/1.0 404 Not Found");
-        }
-    }
-}
-elseif (startsWith($strRequestPath, '/themes/')) {
-    include_once($arrayModules['dbThemes']);
-    if ($strRequestPath == '/themes/') {
-        header('Content-Type: text/plain');
-        foreach ($arrayThemesDB as $_key => $_value) {
-            var_dump(funcReadManifest('theme', $_value, true, false, false, false, false));
-        }
-    }
-    else {
-        $strStrippedPath = str_replace('/', '', str_replace('/themes/', '', $strRequestPath));
-        $ArrayDBFlip = array_flip($arrayThemesDB);
-
-        if (array_key_exists($strStrippedPath,$ArrayDBFlip)) {
-            header('Content-Type: text/plain');
-            var_dump(funcReadManifest('theme', $strStrippedPath, true, true, false, false, false));
-        }
-        else {
-            header("HTTP/1.0 404 Not Found");
-        }
-    }
-}
-elseif ($strRequestPath == '/searchplugins/') {
-    include_once($arrayModules['dbSearchPlugins']);
-    header('Content-Type: text/plain');
-    asort($arraySearchPluginsDB);
-    var_dump($arraySearchPluginsDB);
-}
-else {
-    header("HTTP/1.0 404 Not Found");
-}
-*/
 
 // ============================================================================
 ?>
