6490dc437d7a37a285def3ed708d0d2b36c73706
Matt A. Tobin <email@mattatobin.com>
2017-09-26 23:38:00 -0400

    Add a bit of extra info to AUS component

--- frontend/components/aus.php
+++ frontend/components/aus.php
@@ -3,6 +3,34 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL
 
+// == | INFO | ================================================================
+
+// Automatic Update Service for Add-ons responds with a valid RDF file
+// containing update information for known add-ons or sends the request
+// off to AMO (for now) if it is unknown to us.
+
+// FULL GET Arguments for AUS are as follows:
+
+// [query]          [Description]       [Example]                       [Used]
+// ----------------------------------------------------------------------------
+// reqVersion       Request Version     '2'                             false
+// id               Add-on ID           '{GUID}' or 'user@host.tld'     true
+// version          Add-on Version      '1.2.3a1'                       amo
+// maxAppVersion                        '26.5.0'                        false
+// status           Add-on Status       'userEnabled'                   false
+// appID            Client ID           'toolkit@mozilla.org'           true
+// appVersion       Client Version      '27.2.1'                        true
+// appOS            Client OS           'WINNT'                         false
+// appABI           Client ABI          'x86-gcc3'                      false
+// locale           Client Locale       'en-US'                         false    
+// currentAppVersion                    '27.4.2'                        false
+// updateType       Update Type         '32' or '64'                    false
+// compatMode       Compatibility Mode  'normal', 'ignore', or 'strict' amo
+
+// See: https://developer.mozilla.org/Add-ons/Install_Manifests#updateURL
+
+// ============================================================================
+
 // == | Vars | ================================================================
 
 $boolMozXPIUpdate = false;
